8fd7fd87a1222434633a4cb1d948399dd3bbb44a
Bentley G. Murphy <bentleygmurphy@Bentleys-MacBook-Pro.local>
2019-10-09 09:45:51 -0600

    Added global scss, further structured javascript and added basic layout

--- js/master.js
+++ js/master.js
@@ -1,17 +1,51 @@
 class toDo {
-    constructor(title) {
-        this.title = title;
-        this.list = [
-            this.listItem = {
-                itemContent = "",
-                itemSelected = false,
-            }
-        ]
-    }
+  constructor(title) {
+    this.id = Number();
+    this.title = title;
+    this.selected = false;
+    this.list = [
+      // this.listItem = {
+      //     this.id = Number(),
+      //     itemContent = "",
+      //     itemSelected = false,
+      // }
+    ];
+    this.completedList = [];
+  }
+  addListItem(content) {
+    //list push listItem {-content -selected}
+  }
+  completeSelected(items) {}
+  removeCompleted(items) {}
 }
 
-let objects = [];
+let toDos = [];
 
 //for every entry put todo into objects
 
-//
+//for every listItem show div
+
+function deleteBtn(id, target) {
+  //btn id
+  //target to delete
+}
+
+function addBtn(btnId, impId) {
+  $(btnId).click(function() {
+    $(btnId).hide();
+    $(impId).val("");
+    $(impId).show();
+    $(impId).focus();
+  });
+  $(impId).keypress(function(e) {
+    if (e.which == 13) {
+      $(btnId).show();
+      $(impId).hide();
+      //$('form#login').submit();
+      //create new object here
+      return false;
+    }
+  });
+}
+
+addBtn("#newToDo", "#nameToDo");
